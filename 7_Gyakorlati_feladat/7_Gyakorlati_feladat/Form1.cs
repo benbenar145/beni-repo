09fe891681691f33b3f5a1100c053a25bc265802
ben ben <benbenar145@gmail.com>
2019-12-01 11:25:34 +0100

    harmadik_commit

--- 7_Gyakorlati_feladat/7_Gyakorlati_feladat/Form1.cs
+++ 7_Gyakorlati_feladat/7_Gyakorlati_feladat/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _7_Gyakorlati_feladat.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,28 @@ namespace _7_Gyakorlati_feladat
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName; // label1
             label2.Text = Resource1.FirstName; // label2
             button1.Text = Resource1.Add; // button1
+
+            // listbox1 - listUsers
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
